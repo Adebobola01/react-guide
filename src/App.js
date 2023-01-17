6a44b60ae3eb177770151680ab89247057a3acc9
Adebobola01 <m.oyedunmade@gmail.com>
2023-01-18 00:05:17 +0100

    switched to function components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import Person from "./Person/Person"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Adebobola's first time</h1>
+      <Person/>
     </div>
   );
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,5 @@
+const person = () => {
+    return "I'm getting too old for this shit!"
+}
+
+export default person;
