5b005da571bcaf497e5731de4044464d093e78c8
Adebobola01 <m.oyedunmade@gmail.com>
2023-01-19 16:49:55 +0100

    added state and switched to class

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
+import React, {Component} from "react";
 import Person from "./Person/Person"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Adebobola's first time</h1>
-      <Person name="Adebobola" age="21" />
-      <Person name="Remilekun" age="21" />
-      <Person name="Oyedunmade" age="21" />
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    persons: [
+      { name: "Adebobola", age: 21 },
+      { name: "Remilekun", age: 21 },
+      {name: "Oyedunmade", age: 21}
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Adebobola's first time</h1>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} >I like anime</Person>
+      </div>
+    )
+  };
 }
 
 export default App;
