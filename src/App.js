627209f0cfe60108f9449c0137a43475f0e476a5
Adebobola01 <m.oyedunmade@gmail.com>
2023-01-19 23:50:55 +0100

    added inline styles

--- src/App.js
+++ src/App.js
@@ -30,11 +30,17 @@ class App extends Component {
     ]})
   }
 
+  
   render() {
+    const style = {
+      border: "1px solid red",
+      backgroundColor: "white",
+      padding: "8px"
+    }
     return (
       <div className="App">
         <h1>Adebobola's first time</h1>
-        <button onClick={this.switchNameHandler.bind(this, "bobo")} >switch</button>
+        <button onClick={this.switchNameHandler.bind(this, "bobo")} style={style}>switch</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={ this.nameChangedHandler}  />
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} clicked={this.switchNameHandler.bind(this, "muhy")} >I like anime</Person>
